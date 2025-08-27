47f4be0a03e742a6624c19c00dc4d05fea96810e
Bilalnoori7 <152948220+Bilalnoori7@users.noreply.github.com>
2025-08-26 20:09:18 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/PersonalPortfolio/',
+  base: 'https://bilalx.tech/',
   assetsInclude: ['**/*.glb'] // Add this line to handle GLB files
 });
