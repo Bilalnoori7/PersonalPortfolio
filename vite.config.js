800e1ce6e20a99692393d2c4740a6b321f824767
Bilalnoori7 <152948220+Bilalnoori7@users.noreply.github.com>
2025-08-26 20:04:31 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   base: '/PersonalPortfolio/',
-  assetsInclude: ['**/*.glb']
-})
+  assetsInclude: ['**/*.glb'] // Add this line to handle GLB files
+});
